a4acba85472463642e12cbc3574a3810f87efbec
Rashid Shamloo <rashidshamloo@gmail.com>
2023-04-29 15:42:44 +0330

    Removed unused declaration

--- index.d.ts
+++ index.d.ts
@@ -40,19 +40,4 @@ export const fetchMSAlt: (
   init?: RequestInitMS
 ) => Promise<Response>;
 
-export const multiSignal: {
-  /**
-   * Merges multiple AbortSignals into a single signal. returned signal will be aborted if any of the input signals are aborted.
-   *
-   * MultiSignal ( signal1, signal2, ... ) : AbortSignal
-   */
-  (...signals: AbortSignal[]): AbortSignal;
-  /**
-   * Merges multiple AbortSignals into a single signal. returned signal will be aborted if any of the input signals are aborted.
-   *
-   * MultiSignal ( [ signal1, signal2, ... ] ) : AbortSignal
-   */
-  (...signals: [AbortSignal[]]): AbortSignal;
-};
-
 export default fetchMS;
